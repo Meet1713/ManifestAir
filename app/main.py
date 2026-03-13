f1f17087577a7157bf710719db89c7b04f6aac7c
meetbhesaniya <meetbhesaniyak1713@gmail.com>
2026-03-12 22:43:10 -0600

    did some minimal changes to the code to see working app in the render deployment

--- app/main.py
+++ app/main.py
@@ -3,5 +3,29 @@ from flask import Blueprint, render_template
 bp = Blueprint("main", __name__)
 
 @bp.route("/")
-def home():
+def index():
     return render_template("main/index.html")
+
+@bp.route("/about")
+def about():
+    return "<h1>About page coming soon</h1>"
+
+@bp.route("/careers")
+def careers():
+    return "<h1>Careers page coming soon</h1>"
+
+@bp.route("/press")
+def press():
+    return "<h1>Press page coming soon</h1>"
+
+@bp.route("/help-center")
+def help_center():
+    return "<h1>Help Center page coming soon</h1>"
+
+@bp.route("/terms")
+def terms():
+    return "<h1>Terms of Service page coming soon</h1>"
+
+@bp.route("/privacy")
+def privacy():
+    return "<h1>Privacy Policy page coming soon</h1>"
