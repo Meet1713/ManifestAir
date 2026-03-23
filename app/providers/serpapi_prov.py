94b44b9efa6d16d9a3294f4e2578741e7545390c
meetshahx1 <shahmeet2904@gmail.com>
2026-03-23 16:25:01 -0600

    Library no longer needed

--- app/providers/serpapi_prov.py
+++ app/providers/serpapi_prov.py
@@ -2,7 +2,7 @@ import os
 import serpapi
 from app.db import get_db
 from app.airport_service import AirportDatabase
-from app.airline_links import get_airline_link
+# from app.airline_links import get_airline_link
 
 # Initialize airport database
 airport_db = AirportDatabase()
